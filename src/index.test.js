48e9353187bfa7744c3f5f3b20f8311048d635e5
Fer Bueno <ferbuenob@gmail.com>
2020-05-18 11:30:20 -0500

    Ignore the TGZ and fixed tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.';
+import Mifiel from '.';
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy();
+    expect(Mifiel).toBeTruthy();
   });
 });
